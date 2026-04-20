c1d260515694f7cde58fd0ce9cb47d037f0390d6
icy_w <icy_wcz@sjtu.edu.cn>
2026-04-20 14:26:17 +0800

    优化llm_sft_demo.py中的提示构建和数据填充逻辑，增强代码注释以提高可读性

--- llm_sft_demo.py
+++ llm_sft_demo.py
@@ -33,22 +33,22 @@ class SFTDataset(Dataset):
         input=example.get('input','') #使用get方法获取输入内容，如果没有输入字段，则默认为空字符串，这样可以确保在构建提示时不会出现错误，并且可以处理没有输入的情况。
         output=example['output']
 
-        prompt=f'###User:\n{instruction}\n{input}\n\n###Assistant:\n{output}\n'
+        prompt=f'###User:\n{instruction}\n{input}\n\n###Assistant:\n{output}\n'  #拼接成prompt字段，包含用户的指令、输入和助手的回答，这样可以为模型提供足够的上下文信息，以便进行监督学习微调。使用特定的格式（例如###User:和###Assistant:）可以帮助模型更好地理解不同角色之间的关系，从而提高生成回答的质量。
         answer=output
 
-        prompt_ids=self.tokenizer.encode(prompt,add_special_tokens=False)
+        prompt_ids=self.tokenizer.encode(prompt,add_special_tokens=False) #解码成ids
         answer_ids=self.tokenizer.encode(answer,add_special_tokens=False)
 
         if self.tokenizer.eos_token_id is not None:
-            answer_ids.append(self.tokenizer.eos_token_id)
+            answer_ids.append(self.tokenizer.eos_token_id) #选择是否添加结束标记，如果分词器定义了结束标记（eos_token_id），则将其添加到答案ID的末尾，以便模型在生成回答时知道何时停止生成。
 
         input_ids=prompt_ids+answer_ids
         if len(input_ids)>self.max_length:
-            input_ids=input_ids[:self.max_length]
+            input_ids=input_ids[-self.max_length:] #若超过最大序列长度，就截断输出id，保留最后的max_length个标记，这样可以确保输入序列的长度不会超过模型的最大处理能力，同时保留了最相关的上下文信息。
 
         labels=[-100]*len(prompt_ids)+answer_ids
         if len(labels)>self.max_length:
-            labels=labels[:self.max_length]
+            labels=labels[-self.max_length:]
 
         return {
             'input_ids': torch.tensor(input_ids,dtype=torch.long),
@@ -58,18 +58,18 @@ class SFTDataset(Dataset):
 dataset=SFTDataset('./data/alpaca_data_cleaned.json',tokenizer,256)
 
 
-def collate_fn(batch,pad_id=tokenizer.eos_token_id,label_pad_id=-100):
-    max_len=max(x['input_ids'].numel() for x in  batch)
+def collate_fn(batch,pad_id=tokenizer.eos_token_id,label_pad_id=-100):  #用于将__getitem__获得的一个样本整合成一个批次的数据
+    max_len=max(x['input_ids'].numel() for x in batch)
     input_ids=[]
     labels=[]
     for x in batch:
         ids=x['input_ids']
         lab=x['labels']
-        pad_len=max_len-len(ids)
+        pad_len=max_len-len(ids) #计算每个批次中需要填充的长度，确保每一批次的长度相等
         input_ids.append(torch.cat([ids,torch.full((pad_len,),pad_id,dtype=torch.long)]))
         labels.append(torch.cat([lab,torch.full((pad_len,),label_pad_id,dtype=torch.long)]))
 
-    return {'input_ids': torch.stack(input_ids), 'labels': torch.stack(labels)}
+    return {'input_ids': torch.stack(input_ids), 'labels': torch.stack(labels)} #将ids和lab堆叠成一个批次的数据，返回一个字典，包含输入ID和标签的批次数据，这些数据将用于模型的训练过程。
 
 dataloader=DataLoader(dataset,batch_size=4,shuffle=True,collate_fn=collate_fn)
 
