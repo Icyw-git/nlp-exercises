1a947fab16dde708c2280c29541b613edced0559
icy_w <icy_wcz@sjtu.edu.cn>
2026-04-20 18:03:00 +0800

    优化llm_generate_demo.py中的tokenizer使用，修复输入处理逻辑并改进预测结果获取方式

--- llm_generate_demo.py
+++ llm_generate_demo.py
@@ -3,7 +3,6 @@ from transformers import AutoTokenizer
 
 
 from llm_LLaMA2 import ModelConfig,Transformer
-tokenizer = AutoTokenizer.from_pretrained('tokenizer')
 
 
 @torch.inference_mode()
@@ -16,31 +15,30 @@ def generate_eos(inputs,check_point_path:str,tokenizer,temperature:float,top_k:i
     model.eval()
 
     prompt=f"###User:\n{inputs}\n\n###Assistant:\n"
-    input_ids=tokenizer(prompt,return_tensors='pt',add_special_tokens=False).to(device)
+    input_ids=tokenizer(prompt,return_tensors='pt',add_special_tokens=False)['input_ids'].to(device)
 
-    if tokenizer.eos_token_id is not None:
-        input_ids['input_ids'] = torch.cat([input_ids, torch.tensor([tokenizer.eos_token_id], device=device)], dim=-1)
+
 
     if input_ids.numel() > max_length:
-        input_ids = input_ids[-max_length:]
+        input_ids = input_ids[:,-max_length:]
 
     for _ in range(max_length):
         outputs=model(input_ids)
-        logits=outputs.logits
+        logits=outputs.logits[:,-1,:] #获取最后一个位置的logits，表示下一个标记的预测结果
 
         if temperature==0.0:
-            _,idx=torch.topk(logits,1)
+            idx=torch.argmax(logits,dim=-1,keepdim=True)
 
         else:
             next_token_logits = logits / temperature
-            v,_=torch.topk(next_token_logits,top_k)
-            next_token_logits=next_token_logits.masked_fill[next_token_logits<v[:,[-1]], float('-inf')]
+            v,_=torch.topk(next_token_logits,min(top_k,next_token_logits.size(-1)))
+            next_token_logits=next_token_logits.masked_fill(next_token_logits<v[:,[-1]], float('-inf'))
             probs=torch.softmax(next_token_logits,dim=-1)
             idx=torch.multinomial(probs,num_samples=1)
 
-        input_ids=torch.cat([input_ids,torch.tensor(idx,device=device)],dim=-1)
+        input_ids=torch.cat([input_ids,idx],dim=-1)
 
-        if idx==tokenizer.eos_token_id:
+        if idx.item()==tokenizer.eos_token_id:
             break
 
     return tokenizer.decode(input_ids,skip_special_tokens=True)
@@ -50,6 +48,8 @@ def generate_eos(inputs,check_point_path:str,tokenizer,temperature:float,top_k:i
 
 
 if __name__=='__main__':
+    tokenizer = AutoTokenizer.from_pretrained('tokenizer')
+
     generate_eos('今天天气怎么样？','check_point_path',tokenizer,temperature=0.7,top_k=50)
 
 
